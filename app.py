148e71ce96204cef6857ccdc6eb305000b94819f
MadBananaUnion <madbananabrands@gmail.com>
2024-01-26 12:03:46 -0500

    app.py afixed with the IP address of the targeted local hosting machine that will serve this application, code, and assets, as well as receive https requests

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the Flask app on https://192.168.12.140:5000
+    app.run(debug=True, host='192.168.12.140', port=5000, ssl_context='adhoc')
